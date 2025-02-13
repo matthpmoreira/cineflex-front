041660e601e55e7ac7b58a5fc99dca59f4ee0b03
Matheus Henrique <matthpmoreira@gmail.com>
2025-02-13 05:00:19 -0300

    Run formatter

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "./reset.css";
-import "./utils/onpushstate.js"
+import "./utils/onpushstate.js";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
