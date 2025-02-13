22d99a8771b3c38d5ef37f37f26757a521f39c75
Matheus Henrique <matthpmoreira@gmail.com>
2025-02-13 03:14:10 -0300

    Delete NewPoster component and embed it in Movies component

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 
 import { fetchMovies } from "../utils/backend.js";
-import NewPoster from "./NewPoster.jsx";
 import SelectionRow from "./SelectionRow.jsx";
+import styled from "styled-components";
 
 export default function Movies(props) {
     const [movies, setMovies] = useState(null);
@@ -13,9 +13,50 @@ export default function Movies(props) {
 
     return (
         <SelectionRow title="Selecione um filme" {...props}>
-            {movies?.map(movie => (
-                <NewPoster key={movie._id} {...movie} />
+            {movies?.map((movie) => (
+                <Poster key={movie._id}>
+                    <Image $poster={movie.poster} />
+                    <Title>{movie.title}</Title>
+                </Poster>
             ))}
         </SelectionRow>
     );
 }
+
+const Title = styled.div`
+    color: white;
+    filter: opacity(0);
+    text-align: center;
+    user-select: none;
+
+    transform: translate(-50%, -50%);
+    position: absolute;
+    left: 50%;
+    top: 50%;
+`;
+
+const Image = styled.div`
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    background-image: url(${({$poster}) => $poster});
+    background-position: center;
+    background-size: cover;
+`;
+
+const Poster = styled.div`
+    width: 200px;
+    aspect-ratio: 3 / 4;
+    position: relative;
+    flex-shrink: 0;
+
+    &:hover {
+        ${Image} {
+            filter: brightness(0.6);
+        }
+
+        ${Title} {
+            filter: opacity(1);
+        }
+    }
+`;
