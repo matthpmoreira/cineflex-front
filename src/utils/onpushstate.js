0b6206efbb9ad0cf6238f3a674995f6256ed4e60
Matheus Henrique <matthpmoreira@gmail.com>
2025-02-15 11:48:34 -0300

    Remove `pushState` monkey patch

--- src/utils/onpushstate.js
+++
@@ -1,8 +0,0 @@
-// Monkey patch so that pushing a state dispatches an event
-
-window.history.pushState = new Proxy(window.history.pushState, {
-    apply: function (target, thisArg, args) {
-        target.apply(thisArg, args);
-        window.dispatchEvent(new CustomEvent("onpushstate", args));
-    },
-});
